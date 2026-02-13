4c3fcc92407bb583123200325a5fc9d0bd8e79cc
Joshua Gibson <180050831+yumakuga-afk@users.noreply.github.com>
2026-02-13 13:11:38 -0500

    added more simulation engine components

---
+++ src/CpuSchedulingSim/Simulation/SimConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace CpuSchedulingSim.Simulation;
+
+public sealed class SimConfig
+{
+    public int Seed { get; set; } = 1;
+    public int EndTime { get; set; } = 500;
+
+    public int ContextSwitchCost { get; set; } = 1;
+
+    public double CacheHitProbability { get; set; } = 0.9;
+    public int CacheMissPenalty { get; set; } = 3;
+
+    public int MeanInterarrivalTime { get; set; } = 6;
+    public int ProcessCountTarget { get; set; } = 50;
+
+    public string SchedulerName { get; set; } = "Unknown";
+}
---
+++ src/CpuSchedulingSim/Simulation/SimEvent.cs
@@ -0,0 +1,23 @@
+﻿namespace CpuSchedulingSim.Simulation;
+
+public enum EventType
+{
+    Arrival,
+    Dispatch,
+    SliceComplete,
+    ProcessComplete
+}
+
+public abstract class SimEvent
+{
+    public int Time { get; }
+    public EventType Type { get; }
+
+    protected SimEvent(int time, EventType type)
+    {
+        Time = time;
+        Type = type;
+    }
+
+    public abstract void Execute(SimulationEngine engine);
+}
