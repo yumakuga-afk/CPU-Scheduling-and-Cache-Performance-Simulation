380c5a9e7eda65f39d4d0af9d2f79d92f36c4041
Joshua Gibson <180050831+yumakuga-afk@users.noreply.github.com>
2026-03-16 13:06:01 -0400

    Class added to help parse results

---
+++ src/CpuSchedulingSim/Metrics/ProcessResults.cs
@@ -0,0 +1,15 @@
+﻿namespace CpuSchedulingSim.Metrics;
+
+public sealed class ProcessResult
+{
+    public int Pid { get; init; }
+    public int ArrivalTime { get; init; }
+    public int BurstTime { get; init; }
+    public int Priority { get; init; }
+
+    public int? FirstStartTime { get; init; }
+    public int? FinishTime { get; init; }
+
+    public int WaitingTime { get; init; }
+    public int TurnaroundTime { get; init; }
+}
